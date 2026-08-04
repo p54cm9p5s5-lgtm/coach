fe084fe44f6c745c683ba3a7f0fafd8fd753b6a2
Riccardo Ighina <deuscanide@192.168.1.52>
2026-08-04 09:35:27 +0200

    Ricaricare lo stesso brief non annulla piu' le proposte accettate

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Ogni pubblicazione cambia VERSION: la nuova versione prende il comando
    subito e i file si aggiornano da soli, senza conferme da toccare. */
 
-const VERSION = "20260804-093430";
+const VERSION = "20260804-093527";
 const CACHE = `coach-${VERSION}`;
 
 const ASSETS = [
